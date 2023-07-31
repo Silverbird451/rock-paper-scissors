259b9c9a7f77752b964c5744eeb32ccd94dbc0dc
Silverbird451 <127433606+Silverbird451@users.noreply.github.com>
2023-07-30 22:07:08 -0300

    Rock-Paper-Scissors game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rock-paper-scissors
 A simple rock paper scissors game.
+Project: Rock Paper Scissors in The Odin Project.
+
---
+++ script.js
@@ -0,0 +1,72 @@
+let computerScore = 0;
+let playerScore = 0;
+
+function playRound(){
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    if (computerChoice === "rock" &&  playerChoice === "scissors"){
+        computerScore += 1
+        console.log(`You lost the round, your score: ${playerScore}/5, computer score: ${computerScore}/5`)
+
+    } else if (computerChoice === "paper" &&  playerChoice === "rock"){
+        computerScore += 1
+        console.log(`You lost the round, your score: ${playerScore}/5, computer score: ${computerScore}/5`)
+
+    } else if (computerChoice === "scissors" &&  playerChoice === "paper"){
+        computerScore += 1
+        console.log(`You lost the round, your score: ${playerScore}/5, computer score: ${computerScore}/5`)
+    
+    } else if (playerChoice === "paper" &&  computerChoice=== "rock"){
+        playerScore += 1
+        console.log(`You won the round, your score: ${playerScore}/5, computer score: ${computerScore}/5`)
+
+    } else if (playerChoice === "rock" &&  computerChoice=== "scissors"){
+        playerScore += 1
+        console.log(`You won the round, your score: ${playerScore}/5, computer score: ${computerScore}/5`)
+
+    } else if (playerChoice === "scissors" &&  computerChoice=== "paper"){
+        playerScore += 1
+        console.log(`You won the round, your score: ${playerScore}/5, computer score: ${computerScore}/5`)
+    } else {
+        console.log("It is a draw!")
+    }
+    checkScores()
+}
+
+function checkScores(){
+    if (computerScore === 5){
+        console.log("The computer has won the game!!")
+    } else if (playerScore === 5){
+        console.log("The player has won the game!!")
+    } else {
+        playRound()
+    }
+    
+}
+
+function getComputerChoice(){
+    let randomNum = 1 + Math.floor(Math.random() * 3);
+    let choice;
+    if (randomNum === 1){
+        choice = "rock"
+    } else if (randomNum === 2){
+        choice = "paper"
+    }else if (randomNum === 3){
+        choice = "scissors"
+    }
+    return choice;
+}
+
+function getPlayerChoice(){
+    let playerInput = prompt("Chose between: Rock, Paper or Scissors");
+    let choice = playerInput.toLowerCase();
+    if ((choice === "paper") || (choice === "rock") || (choice === "scissors")){
+        return choice; 
+    } else {
+        console.log("Option not valid.")
+        getPlayerChoice()
+    }
+}
+
+checkScores()
